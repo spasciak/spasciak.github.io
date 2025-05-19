b9090008089256a8eeef94b4c74fa125856f3bbb
spasciak <88434334+spasciak@users.noreply.github.com>
2025-05-19 18:52:29 +0200

    Update consent-popup.js

--- consent-popup.js
+++ consent-popup.js
@@ -1,5 +1,5 @@
 (function() {
-  if (document.cookie.split('; ').some(row => row.startsWith('tou-pp_agreed=true='))) return;
+  if (document.cookie.split('; ').some(row => row.startsWith('tou-pp_agreed='))) return;
 
   // Get the script tag that loaded this file
   const currentScript = document.currentScript || (function() {
